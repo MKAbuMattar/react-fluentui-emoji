ff004ec4abf17b2ae38624dd6838c035a8e12c14
MKAbuMattar <mohammad.khaled@outlook.com>
2024-08-24 16:17:38 +0300

    chore: Update dependencies and Optimize code

---
+++ script/git-clone.config.ts
@@ -0,0 +1,22 @@
+import {exec} from 'node:child_process';
+
+const gitClone = (url: string): void => {
+  exec(url, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.debug(`stderr: ${stderr}`);
+      return;
+    }
+    console.debug(`stdout: ${stdout}`);
+  });
+};
+
+(() => {
+  console.log('Cloning fluentui-emoji...');
+  gitClone(
+    'git clone --depth=1 git@github.com:MKAbuMattar/private-fluentui-emoji.git',
+  );
+})();
